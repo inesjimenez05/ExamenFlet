e5d056f81594d9976400c60c020087f25a286db1
Inés <al_ijimenezg@iessierradelasvillas.org>
2023-03-03 11:14:58 +0100

    Examen

--- Login.py
+++ Login.py
@@ -2,43 +2,31 @@ import flet as ft
 
 
 def main(page: ft.Page):
+
+    def registro (e):
+        nombre=tfnombre.value
+        contraseña=tfpassword.value
+        if nombre == contraseña:
+            dlg = ft.AlertDialog(title=ft.Text("Contraseña y usuario correctos"), on_dismiss=lambda e: print("Dialog dismissed!"))
+            page.dialog=dlg
+            dlg.open = True
+        page.update()
+
     page.title = "Examen Flet"
 
-    #Ejercicio 6
-
-    #Fin --- Ejercicio 6
-
-
-    #Ejercicio 2
-    
-    
-    #Fin --- Ejercicio 2
-
-
-    #Ejercicio 3
-    
-
-    #Fin --- Ejercicio 3
-
+    page.horizontal_alignment= ft.CrossAxisAlignment.CENTER
     page.window_height=500
     page.window_width= 250
 
-
-
+    img=ft.Image(src="fotos/Logo.png",width=100,height=100)
+    page.update()
+      
     tfnombre = ft.TextField(label="Nombre")
-    #Ejercicio 4
+    tfpassword= ft.TextField(label="Contraseña",password=True, can_reveal_password=True)
+    boton=ft.ElevatedButton("Registrarse", icon="add", on_click=registro)
     
 
-    #Fin --- Ejercicio 4
-
-
-    #Ejercicio 5
-
-    #Fin-- Ejercicio 5
-
-
-    page.add(img,tfnombre,tfpassword,btnEntrar)
+    page.add(img,tfnombre,tfpassword,boton)
     
 
-
-ft.app(target=main,assets_dir="imagenes")
+ft.app(target=main,assets_dir="fotos")
